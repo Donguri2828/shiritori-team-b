3621453b7ef7b2b9f7a111206364cc2efc66bfa0
Donguri2828 <donguri.gamefan@gmail.com>
2024-10-04 10:26:00 +0900

    go, restartボタンの実装とonload処理の実装

--- public/script.js
+++ public/script.js
@@ -1,7 +1,20 @@
+window.onload = async (event) => {
+  // GET /shiritoriを実行
+  const response = await fetch("/prev-word", { method: "GET" });
+  // responseの中からレスポンスのテキストデータを取得
+  const previousWord = await response.text();
+  // id: previousWordのタグを取得
+  const paragraph = document.querySelector("#first");
+  // 取得したタグの中身を書き換える
+  paragraph.innerHTML = `${previousWord}`;
+}
+
 let timerInterval;
 let minutes;
 let seconds;
 let logs = [];
+const smallHiragana = ["ぁ", "ぃ", "ぅ", "ぇ", "ぉ", "ゃ", "ゅ", "ょ", "ゎ", "っ"];
+
 function starttime() {
   // カウントダウンを開始する前にタイマーが動いていればクリア
   clearInterval(timerInterval);
@@ -38,25 +51,66 @@ function updateTime() {
 }
 // starttime();
 
-function restart() {
-  logs = [];
-  document.getElementById("left").textContent = "";
-  document.getElementById("first").textContent = "りょ";
-  document.getElementById("word").value = "";
-  starttime();
+document.querySelector(".restart").onclick = async(event) => {
+    const response = await fetch("/reset-log", {method: "POST"});
+    const previousWord = await response.text();
+    const paragraph = document.querySelector("#first");
+    paragraph.innerHTML = previousWord;
+    const left = document.querySelector("#left");
+    left.innerHTML = "";
 }
+
 function log() {
   const dialog = document.getElementById("dialog");
   dialog.showModal();
   modalMain();
 }
-function go() {
-  const text1 =
-    document.getElementById("first").textContent +
-    document.getElementById("word").value;
-  console.log(text1);
-  ok(text1);
+
+document.querySelector("#go").onclick = async(event) => {
+  // wordタグを取得
+  const nextWordInput = document.querySelector("#word");
+  // firstタグを取得
+  const firstWord = document.querySelector("#first");
+  // firstとwordの中身を取得
+  const nextWordInputText = firstWord.innerHTML + nextWordInput.value;
+  // POST /shiritoriを実行
+  // 次の単語をresponseに格納
+  const response = await fetch(
+    "/next-word",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nextWord: nextWordInputText })
+    }
+  );
+
+  // status: 200以外が返ってきた場合にエラーを表示
+  if (response.status !== 200) {
+    const errorJson = await response.text();
+    const errorObj = JSON.parse(errorJson);
+    alert(errorObj["errorMessage"]);
+    return;
+  }
+
+  const previousWord = await response.text();
+
+  // id: previousWordのタグを取得
+  const paragraph = document.querySelector("#left");
+  // 取得したタグの中身を書き換える
+  paragraph.innerHTML = `${previousWord}`;
+
+  // firstタグの中身を書き換える
+  if (smallHiragana.includes(previousWord.slice(-1))) {
+      firstWord.innerHTML = previousWord.slice(-2);
+  }
+  else {
+      firstWord.innerHTML = previousWord.slice(-1);
+  }
+
+  // inputタグの中身を消去する
+  nextWordInput.value = "";
 }
+
 function ok(word) {
   document.getElementById("word").value = "";
   changeLeft(word);
